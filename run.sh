128f07cd4905601992e789baec6957f74aa65670
TianFeng <31143993+lovelytt0@users.noreply.github.com>
2021-10-22 11:44:04 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,9 @@ conda env update -f=env.yml -n base
 conda env create -f env.yml
 
 # install Eqtransformer
+cd ..
 git clone --recurse-submodules git://github.com/smousavi05/EQTransformer
+cd EQTransformer
 python setup.py install
 
 
