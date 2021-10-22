c0bb4ceb590c1c1335f3ee84e8fd2508437df09c
TianFeng <31143993+lovelytt0@users.noreply.github.com>
2021-10-22 11:42:12 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
+
+# install PhaseNet
 git clone --recurse-submodules   https://github.com/wayneweiqiang/PhaseNet
+cd PhaseNet
+conda env update -f=env.yml -n base
+conda env create -f env.yml
+
+# install Eqtransformer
+git clone --recurse-submodules git://github.com/smousavi05/EQTransformer
+python setup.py install
 
 
 
