dbe65eda33a7cb7247800c23660f00c68b02087d
TianFeng <31143993+lovelytt0@users.noreply.github.com>
2021-10-23 16:33:44 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ cd PhaseNet
 echo "install phasenet env"
 conda env update -f=env.yml -n base
 conda env create -f env.yml
+conda activate phasenet
+ipython kernel install --user --name=phasenet
+conda deactivate
 
 # install Eqtransformer
 cd ..
@@ -14,4 +17,9 @@ echo "install EQTransformer env"
 python setup.py install
 
 
-
+conda create -n eqt python=3.7
+conda activate eqt
+conda install -c smousavi05 eqtransformer
+#conda install tensorflow-gpu==2.2.0 
+ipython kernel install --user --name=eqtransformer
+conda deactivate
